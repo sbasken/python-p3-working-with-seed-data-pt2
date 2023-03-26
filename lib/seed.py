0f370261c689f847e971620bb5e9f9b6b567c7f4
Saki <sbasken@gmail.com>
2023-03-26 13:24:19 -0700

    seed data

--- lib/seed.py
+++ lib/seed.py
@@ -29,7 +29,36 @@ def create_users():
     session.commit()
     return users
 
+
 if __name__ == '__main__':
     games = create_games()
     reviews = create_reviews()
     users = create_users()
+
+####
+# def delete_records():
+#     session.query(Game).delete()
+#     session.query(Review).delete()
+#     session.query(User).delete()
+#     session.commit()
+
+# def create_records():
+#     games = [Game() for i in range(100)]
+#     reviews = [Review() for i in range(1000)]
+#     users = [User() for i in range(500)]
+#     session.add_all(games + reviews + users)
+#     session.commit()
+#     return games, reviews, users
+
+# def relate_records(games, reviews, users):
+#     for review in reviews:
+#         review.user = rc(users)
+#         review.game = rc(games)
+
+#     session.add_all(reviews)
+#     session.commit()
+
+# if __name__ == '__main__':
+#     delete_records()
+#     games, reviews, users = create_records()
+#     relate_records(games, reviews, users)
